5645970f7cb0f7efa93b5eeca7e401168d042051
HWChang <chang.hao-wei@wustl.edu>
2020-06-17 15:42:04 -0500

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Usage:
+# move to one level above emmer/
+# python3 -m emmer.tests
+
+import unittest
+from .troubleshoot.warn.warning import WarningCode6
+
+
+"""
+Initiate all tests
+"""
+
+
+if __name__ == '__main__':
+    # try:
+    #     if path exist
+    #         raise WarningCode6(silence = False)
+    # except:
+    #     inputCodeX    
+
+    testsuite = unittest.TestLoader().discover('.')
+    unittest.TextTestRunner(verbosity=1).run(testsuite)
